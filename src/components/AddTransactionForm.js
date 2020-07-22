745fbac488c0813aebb802dca4aa25c975b36417
JonTDean <JonathanTomDean@Gmail.com>
2020-07-22 10:33:29 -0400

    Finished the AddTransactionForm

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -43,20 +43,50 @@ class AddTransactionForm extends Component {
 
   }
 
+  
+  // Form Changes
+  // Date
+  handleDateChange = e => {
+    this.setState({
+      date: e.target.value
+    })
+  }
+
+  // Description
+  handleDescriptionChange = e => {
+    this.setState({
+      description: e.target.value
+    })
+  }
+
+  // Category
+  handleCategoryChange = e => {
+    this.setState({
+      category: e.target.value
+    })
+  }
+
+  // Amount
+  handleAmountChange = e => {
+    this.setState({
+      amount: e.target.value
+    })
+  }
 
   render() {
     return (
       <div className="ui segment">
         <form className="ui form" onSubmit={this.newTransactionSubmit}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={this.handleDateChange} />
+            <input type="text" name="description" placeholder="Description" onChange={this.handleDescriptionChange} />
+            <input type="text" name="category" placeholder="Category" onChange={this.handleCategoryChange} />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={this.handleAmountChange}
             />
           </div>
           <button className="ui button" type="submit">

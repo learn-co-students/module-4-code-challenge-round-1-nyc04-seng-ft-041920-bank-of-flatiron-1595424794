64892244a3c2c2589b144f2b8a0ecf7606358f56
bashir2017 <bashiralhanshali@yahoo.com>
2020-07-22 10:07:59 -0400

    core

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,43 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  
+  state = {
+    transactions: [],
+    searchTerm: ''
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        transactions: data
+      })
+    })
+  }
+
+  handleSaveData = (newData) => {
+    this.setState(prevState => {
+      return {
+        transactions: [...prevState.transactions, newData]
+      }
+    })
+  }
+
+  setSearchTerm = (term) => {
+    this.setState({
+      searchTerm: term
+    })
+  }
+
   render() {
+    let filterTransactions = this.state.transactions.filter(trans => trans.description.includes(this.state.searchTerm))
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search setSearchTerm={this.setSearchTerm}/>
+        <AddTransactionForm handleSaveData={this.handleSaveData}/>
+        <TransactionsList transactions={filterTransactions}/>
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-const Search = () => {
+const Search = (props) => {
+
+  let onChangeHandler = (event) => {
+    props.setSearchTerm(event.target.value)
+  }
+
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
-        onChange={() => {
-          console.log("Searching...");
-        }}
+        onChange={onChangeHandler}
       />
       <i className="circular search link icon"></i>
     </div>

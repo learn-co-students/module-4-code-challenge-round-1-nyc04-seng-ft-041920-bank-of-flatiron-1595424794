28c3d10f2dfe9fb59996cde3441bd0db17c41e8b
amer519 <afahmy519@gmail.com>
2020-07-22 12:26:33 -0400

    deliverable

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,44 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state = {
+    searchTerm: "",
+    transaction: []
+  }
+  
+  componentDidMount() {
+    fetch("http://localhost:6001/transactions")
+    .then(r => r.json())
+    .then(transaction => {
+      this.setState({
+        transaction: transaction
+      })
+    })
+  }
+
+  addSubmit = newSubmit => {
+    this.setState({
+      transaction: [newSubmit, ...this.state.transaction]
+    })
+  }
+
+  handleTheSearch = searchTerm => {
+    this.setState({
+      searchTerm: searchTerm
+    })
+  }
+
+
+
+
+
   render() {
+    // console.log(this.state)
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchTerm={this.state.searchTerm} setSearchTerm={this.handleTheSearch}/>
+        <AddTransactionForm handleAddSubmit={this.addSubmit}/>
+        <TransactionsList transaction={this.state.transaction}/>
       </div>
     );
   }

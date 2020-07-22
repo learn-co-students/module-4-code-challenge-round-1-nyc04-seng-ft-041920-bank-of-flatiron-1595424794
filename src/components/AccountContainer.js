05fbe78d4585da73bd3cf5c1ad1615732b1fb02e
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-07-22 11:05:35 -0400

    90min

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,15 +4,35 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  
+  
+  state = {
+    allTransactions: []
+  }
+
+  componentDidMount = () => {
+    fetch("http://localhost:6001/transactions")
+      .then(r => r.json())
+      .then(allTransactions => {
+         this.setState({allTransactions})
+      }
+      )
+  }
+
+
+
   render() {
+
+    console.log(this.state)
+
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList allTransactions = {this.state.allTransactions}/>
       </div>
     );
   }
 }
 
-export default AccountContainer;
+export default AccountContainer

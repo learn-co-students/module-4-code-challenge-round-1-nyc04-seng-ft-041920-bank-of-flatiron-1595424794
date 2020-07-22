05cb708b6273532cba939622114dc89ee7013511
jinnic <yoohyojin@gmail.com>
2020-07-22 10:54:26 -0400

    Core deliverables done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,48 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state={
+    transactions: [],
+    searchTerm: ''
+  }
+ 
+  handleSearch = (searchTerm) =>{
+    this.setState({searchTerm: searchTerm},()=>console.log("handleSearch : ",this.state.searchTerm))
+    this.filterTransactions()
+  }
+
+  addTransactions=(transactionObj)=>{
+    this.setState(preState =>({
+      transactions: [transactionObj, ...preState.transactions]
+    }),()=>console.log("addTransaction : ",this.state))
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+      .then(r=>r.json())
+      .then(transArray =>{
+        this.setState({
+          transactions: transArray
+        },()=>console.log("componentDidMount : ",this.state))
+      })
+  }
+
+  filterTransactions=()=>{
+    const filteredTransactions = this.state.transactions.filter(trans => {
+      return trans.description
+      .toLowerCase()
+      .includes(this.state.searchTerm.toLowerCase())
+    })
+    // console.log("Filtered : ", filteredTransactions)
+    return filteredTransactions
+  }
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchTerm={this.state.searchTerm} handleSearch={this.handleSearch} />
+        <AddTransactionForm updateSubmit={this.addTransactions}/>
+        <TransactionsList transactions={this.filterTransactions()}/>
       </div>
     );
   }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Transaction = () => {
+const Transaction = (props) => {
+  const transaction = props.trans
+  // console.log(transaction)
   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{transaction.date}</td>
+      <td>{transaction.description}</td>
+      <td>{transaction.category}</td>
+      <td>{transaction.amount}</td>
     </tr>
   );
 };

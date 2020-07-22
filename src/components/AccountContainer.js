a8b71e78c2d67555345f391e8225d62a84621272
senadakadric <skadric@fordham.edu>
2020-07-22 10:51:05 -0400

    core deliverables done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,45 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state = {
+    transactions: [],
+    searchTerm: "",
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:6001/transactions")
+      .then((resp) => resp.json())
+      .then((transactions) => {
+        this.setState({
+          transactions: transactions,
+        });
+      });
+  }
+
+  addTransaction = (newTransaction) => {
+    this.setState((prevState) => ({
+      transactions: [...prevState.transactions, newTransaction],
+    }));
+  };
+
+  handleSearch = (searchTerm) => {
+    this.setState({
+      searchTerm: searchTerm,
+    });
+  };
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search
+          searchTerm={this.state.searchTerm}
+          handleSearch={this.handleSearch}
+        />
+        <AddTransactionForm handleAddTransaction={this.addTransaction} />
+        <TransactionsList
+          transactions={this.state.transactions}
+          searchTerm={this.state.searchTerm}
+        />
       </div>
     );
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,20 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+  const renderFilteredTransactions = () => {
+    let filteredTransactions = props.transactions.filter((transaction) =>
+      transaction.description.includes(props.searchTerm)
+    );
+    return filteredTransactions;
+  };
+
+  const renderTransactions = () => {
+    return renderFilteredTransactions().map((transaction) => (
+      <Transaction key={transaction.id} transaction={transaction} />
+    ));
+  };
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +32,7 @@ const TransactionsList = () => {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render Transactions here */}
+        {renderTransactions()}
       </tbody>
     </table>
   );
